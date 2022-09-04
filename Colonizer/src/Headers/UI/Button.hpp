937589206bf09d0150f14ab24bf1bccf6ff14689
Swarnim Das <swarnim.das.01@gmail.com>
2022-09-04 14:02:16 +0530

    Improved Button

--- Colonizer/src/Headers/UI/Button.hpp
+++ Colonizer/src/Headers/UI/Button.hpp
@@ -6,30 +6,54 @@ template <class T>
 class Button {
 
 public:
-	Button(sf::FloatRect _r, T* _obj, void (*_a)(Button&, T&)) {
+	Button(sf::FloatRect _r, T* _obj, void (*_onClick)(Button&, T&), void (*_onRelease)(Button&, T&)) {
 		obj = _obj;
 		hitbox = _r;
-		onClick = _a;
+		onClick = _onClick;
+		onRelease = _onRelease;
 
 		shape = sf::RectangleShape(sf::Vector2f(hitbox.width, hitbox.height));
 		shape.setPosition(sf::Vector2f(hitbox.left, hitbox.top));
+
+		sf::Font* f = new sf::Font();
+		if(f->loadFromFile("./Assets/Alice-Regular.ttf")) {
+			text = sf::Text("Button", *f, 40);
+			text.setFillColor(sf::Color::Black);
+			text.setOrigin(text.getLocalBounds().width / 2, text.getLocalBounds().height / 2);
+			text.setPosition(hitbox.left + hitbox.width / 2, hitbox.top + hitbox.height / 3);
+		}
+	}
+
+	~Button() {
+		delete text.getFont();
 	}
 
 public:
 	bool isClicked = false;
+	sf::Color clickedColor = sf::Color(0xaaaaaaff);
+	sf::Color releasedColor = sf::Color(0xffffffff);
+	sf::Text text;
 
 private:
 	T* obj;
 	sf::FloatRect hitbox;
 	sf::RectangleShape shape;
-	sf::Text text;
 	void (*onClick)(Button&, T&);
+	void (*onRelease)(Button&, T&);
 
 public:
 	void Click() {
 		isClicked = true;
-		shape.setFillColor(sf::Color(0xaaaaaaff));
-		onClick(*this, *obj);
+		shape.setFillColor(clickedColor);
+		if(onClick != nullptr)
+			onClick(*this, *obj);
+	}
+
+	void Release() {
+		isClicked = false;
+		shape.setFillColor(releasedColor);
+		if(onRelease != nullptr)
+			onRelease(*this, *obj);
 	}
 
 	sf::FloatRect getHitbox() {
--- Colonizer/src/MainMenu/MainMenu.cpp
+++ Colonizer/src/MainMenu/MainMenu.cpp
@@ -2,8 +2,10 @@
 
 MainMenu::MainMenu() {
 	mWindow = new sf::RenderWindow(sf::VideoMode(800, 600), "Colonizer - Main Menu", sf::Style::Close);
+	mWindow->setFramerateLimit(30);
 
-	Button<MainMenu>* b = new Button<MainMenu>(sf::FloatRect(20, 450, 400, 100), this, &clk1);
+	Button<MainMenu>* b = new Button<MainMenu>(sf::FloatRect(20, 450, 400, 100), this, nullptr, &clk1);
+	b->text.setString("Start");
 	Buttons.push_back(b);
 }
 
@@ -35,19 +37,18 @@ void MainMenu::Loop() {
 					if(mEvents.mouseButton.button == sf::Mouse::Button::Left) {
 						for(auto a : Buttons) {
 							if(a->isClicked) {
-								a->getShape()->setFillColor(sf::Color::White);
-								a->isClicked = false;
+								a->Release();
 							}
 						}
 					}
+					break;
 			}
 		}
 		mWindow->clear();
 		for(auto& a : Buttons) {
-			
 			mWindow->draw(*(a->getShape()));
+			mWindow->draw(a->text);
 		}
-
 		mWindow->display();
 	}
 }
